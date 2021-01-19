51cceaf8d29694e5185645c53f442fc944516dbe
sysb1n <sysb1n@kacer>
2021-01-19 03:26:34 -0300

    mexi numas things here. nem remember o what eu changed mas u olha there kk

--- install.py
+++ install.py
@@ -1,5 +1,3 @@
 import os
-os.system ("clear") 
-os.system("chmod +x lammervault.cpp")
-os.system ("g++ lammervault.cpp")
+os.system ("g++ lammervault.cpp -o lammervault")
 os.system ("./lammervault")
--- lammervault.cpp
+++ lammervault.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 
-int main() {
-  std::cout << "lammervault" << std::endl;
-  return 0;
+using namespace std;
+
+int main() { // a main meu broder!!
+
+	string remmal = "lammer vault B)";
+	/* char *comentario = "isso e um comentario kk"; */
+
+	std::cout << std::string(remmal).c_str() << std::endl;
+	return 0; // ele back para zero porque he é a coidado
+
 }
+
+// ate more ver my consagrado ;)
