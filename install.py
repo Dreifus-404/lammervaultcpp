27338f6c79617be90807af571f8459395c61137d
Dreifus-404 <77207608+Dreifus-404@users.noreply.github.com>
2021-01-19 03:40:53 -0300

    Update install.py

--- install.py
+++ install.py
@@ -1,3 +1,23 @@
-import os
-os.system ("g++ lammervault.cpp -o lammervault")
-os.system ("./lammervault")
+#codi
+def Install():
+            import os
+ os.system('clear')
+      print ('Istalano') #Izo E Pra Apacere Na tela 
+   os.system('chmod +x lammervault.cpp')
+              os.system('g++ lammervault.cpp')
+         os.system('./a.out')
+#ececucao di defi
+install()
+#fin da ececucao
+def Boua_Vynda():
+ print("Ben Vyndo Ao Jogu")
+     a = 10
+  b = 9
+  c = a//b
+      if(c > 100):
+           print(), print()
+       print("Instalação Concluída")
+    else:
+            print(), print()
+       print("Instalação Concluída")
+   
